024d36b030a06fb66feecdde488b3caac8dcf8c3
Khang Bui <khang.bui@wsu.edu>
2025-06-02 12:07:08 -0700

    table(db): added workerarrival table

---
+++ nailspa-backend/src/models/workerarrival.ts
@@ -0,0 +1,19 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+export default (sequelize: Sequelize) => {
+  const WorkerArrival = sequelize.define("WorkerArrival", {
+    workerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    arrivalDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    arrivalOrder: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  return WorkerArrival;
+};
